14b9b9d0382170e487f636fc25db9f801d822ad0
HUYBERIC <huyberic@gmail.com>
2024-11-11 18:53:27 +0100

    Still not responsive at 100%

--- app.js
+++ app.js
@@ -1,20 +1,121 @@
 
 
-// V A R I A B L E S
+// --- H A M B U R G E R ---  //
+
+// v a r i a b l e s
 
 const menuIcon = document.querySelector('#menu-icon')
 const navBar = document.querySelector('.navbar')
 
-// F U N C T I O N S
-
-// MenuHamburger
+// f u n c t i o n s
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navBar.classList.toggle('active')
 }
 
-// TypeJS
+// --- D I S C O R D  B U T T O N S ---  //
+
+// v a r i a b l e s
+
+const discordBtns = document.querySelectorAll('.discordBtn')
+
+// f u n c t i o n s
+
+discordBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const textToCopy = '263325233283334145';
+        navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "ID copied successfully",
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'custom-popup-class',
+                    title: 'custom-title-class',
+                    icon: 'custom-icon-class',
+                }
+            });
+        })
+        .catch(err => {
+            console.error("Copy error", err);
+        });
+    });
+});
+
+
+// --- M O D A L - R E A D M O R E //
+
+// v a r i a b l e s
+
+const modalReadMore = document.querySelector('.modal-read-more')
+const overlay = document.querySelector('.overlay')
+const closingBtn = document.querySelector('.closing-modal')
+const btnReadMore = document.querySelector('.btn-read-more')
+
+// f u n c t i o n s
+
+btnReadMore.addEventListener('click', (event) => {
+    event.preventDefault();
+    openModal();
+});
+
+function handleEscape(event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+        closeModal();
+        closeSkillsModal();
+    }
+}
+
+function openModal() {
+    modalReadMore.classList.remove('hidden');
+    document.documentElement.classList.add('no-scroll');
+}
+
+function closeModal() {
+    modalReadMore.classList.add('hidden');
+    document.documentElement.classList.remove('no-scroll');
+}
+
+btnReadMore.addEventListener('click', openModal);
+closingBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', handleEscape);
+
+// --- M O D A L - S K I L L S //
+
+// v a r i a b l e s
+
+const modalSkills = document.querySelector('.modal-skills')
+const overlaySkills = document.querySelector('.overlay-skills')
+const closingBtnSkills = document.querySelector('.closing-modal-skills')
+const btnSkills = document.querySelector('.btn-skills')
+
+// f u n c t i o n s
+
+btnSkills.addEventListener('click', (event) => {
+    event.preventDefault();
+    openSkillsModal();
+});
+
+function openSkillsModal() {
+    modalSkills.classList.remove('hidden');
+    document.documentElement.classList.add('no-scroll');
+}
+
+function closeSkillsModal() {
+    modalSkills.classList.add('hidden');
+    document.documentElement.classList.remove('no-scroll');
+}
+
+btnSkills.addEventListener('click', openSkillsModal);
+closingBtnSkills.addEventListener('click', closeSkillsModal);
+overlaySkills.addEventListener('click', closeSkillsModal);
+
+// --- T Y P E S . J S --- //
 
 const typed = new Typed(".multiple-text", {
     strings : [
@@ -49,7 +150,5 @@ var swiper = new Swiper(".mySwiper", {
         hide: true,
     },
 });
-
-// E V E N T L I S T E N E R S
 
 
